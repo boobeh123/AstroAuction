7c01f5ab491aa3d3b8b52d703fb7c6e1d4e259d5
Bobby <boobeh321@gmail.com>
2025-08-07 20:34:07 -1000

    Add main Route, update dotenv config, Set EJS as view engine, Serve static assets, Set parsers for url-encoded form bodies & JSON request bodies. Temporarily comment out DB/Auth to render view

--- server.js
+++ server.js
@@ -2,14 +2,40 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const logger = require('morgan');
-const connectDB = require('./config/db')
+// const connectDB = require('./config/db')
 const methodOverride = require('method-override');
 const session = require('express-session');
-const MongoStore = require('connect-mongo')(session)
-const mongoose = require('mongoose');
+// const MongoStore = require('connect-mongo')(session)
+// const mongoose = require('mongoose');
 const passport = require('passport');
+const mainRoutes = require('./routes/main');
 
-dotenv.config();
+require('dotenv').config({path: './config/.env'})
+
+// Passport config
+// require('./config/passport')(passport);
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(logger('dev'));
+//Use forms for put / delete
+app.use(methodOverride("_method"));
+// Sessions
+// app.use(
+//     session({
+//         secret: process.env.SESSION_SECRET,
+//         resave: false,
+//         saveUninitialized: false,
+//         store: new MongoStore({ mongooseConnection: mongoose.connection }),
+//     })
+//   )
+// Passport middleware
+// app.use(passport.initialize())
+// app.use(passport.session())
+
+app.use('/', mainRoutes);
 
 app.listen(process.env.PORT, ()=>{
     console.log('Server is running, you better catch it!')
