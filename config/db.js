e814f576f8b05b78100dd26f96d7cfb73abbce7b
Bobby <boobeh321@gmail.com>
2025-08-09 09:49:13 -1000

    Create db.js and Add connectDB() method. Remove deprecated Mongoose v6 options to align with Mongoose v8

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+
+  try {
+    const conn = await mongoose.connect(process.env.DB_STRING)
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`)
+
+  } catch (err) {
+
+    console.error(err, err.message)
+    process.exit(1)
+
+  }
+
+}
+
+module.exports = connectDB
