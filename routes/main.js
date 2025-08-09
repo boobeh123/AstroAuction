8578163e393ff10c649d8bf2e4c02058e88a78dd
Bobby <boobeh321@gmail.com>
2025-08-09 10:59:20 -1000

    Add two GET routes - /login & /signup

--- routes/main.js
+++ routes/main.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const homeController = require('../controllers/homeController');
+const homeController = require('../controllers/home');
+const authController = require('../controllers/auth');
 
 router.get('/', homeController.getIndex);
+
+router.get('/login', authController.getLogin);
+router.get('/signup', authController.getSignup);
 
 module.exports = router;
