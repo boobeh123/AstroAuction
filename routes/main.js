16051714e1c095b2ce80ee9569785d10ae9cd1e6
Bobby <boobeh321@gmail.com>
2025-08-10 21:56:56 -1000

    Add express-validator. Create ensureAccountValidation. Validate req.body POST requests on /signup route

--- routes/main.js
+++ routes/main.js
@@ -2,11 +2,27 @@ const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/home');
 const authController = require('../controllers/auth');
+const { body } = require('express-validator');
 
 router.get('/', homeController.getIndex);
 
 router.get('/login', authController.getLogin);
 router.get('/signup', authController.getSignup);
-router.post('/signup', authController.postSignup);
+
+const ensureAccountValidation = [
+    body('email')
+    .isEmail().withMessage('Please enter a valid email address.')
+    .normalizeEmail({ gmail_remove_dots: false }),
+    body('password')
+    .isLength({ min: 8 }).withMessage('Password must be at least 8 characters long'),
+    body('confirmPassword')
+    .custom((value, { req }) => {
+        if (value !== req.body.password) throw new Error('Passwords do not match');
+        return true;
+    }),
+    body('agreeToTerms')
+    .equals('yes').withMessage('You must agree to the Terms of Use')
+];
+router.post('/signup', ensureAccountValidation, authController.postSignup);
 
 module.exports = router;
