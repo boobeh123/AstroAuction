dc20c1ff71ce0a3c66f77f41aefe4cdef51c26bf
Bobby <boobeh321@gmail.com>
2025-08-10 21:59:29 -1000

    Update postSignup method - Add conditionals & validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
 const passport = require('passport')
-const validator = require('express-validator')
+const { validationResult } = require('express-validator');
 const User = require('../models/User')
 
 module.exports = {
@@ -14,12 +14,20 @@ module.exports = {
     },
 
     postSignup: async (req, res, next) => {
-//      TODO - validation, success msg, redirect
+
         try {
+            const errors = validationResult(req);
             const { email, password, confirmPassword, agreeToTerms } = req.body;
-            const existingUser = await User.findOne({
-                $or: [{ email: email }, { userName: req.body.userName }]
-              }).lean();
+            const existingUser = await User.findOne({ email: email }).lean();
+
+            if (!errors.isEmpty()) {
+                return res.status(400).redirect('/signup');
+            }
+            
+            if (existingUser) {
+                // req.flash('errors', { msg: 'Account with that email address or username already exists.' });
+                return res.status(400).redirect('/signup');
+            }
 
               const user = new User({
                 email,
@@ -33,43 +41,5 @@ module.exports = {
         } catch (err) {
             return next(err);
         }
-
-        // const validationErrors = []
-        // if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: 'Please enter a valid email address.' })
-        // if (!validator.isLength(req.body.password, { min: 8 })) validationErrors.push({ msg: 'Password must be at least 8 characters long' })
-        // if (req.body.password !== req.body.confirmPassword) validationErrors.push({ msg: 'Passwords do not match' })
-      
-        // if (validationErrors.length) {
-        //   req.flash('errors', validationErrors)
-        //   return res.redirect('../signup')
-        // }
-        // req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false })
-      
-    //     const user = new User({
-    //       email: req.body.email,
-    //       password: req.body.password,
-    //       agreeToTerms: req.body.agreeToTerms
-    //     })
-      
-    //     User.findOne({$or: [
-    //       {email: req.body.email},
-    //       {userName: req.body.userName}
-    //     ]}, (err, existingUser) => {
-    //       if (err) { return next(err) }
-    //       if (existingUser) {
-    //         req.flash('errors', { msg: 'Account with that email address or username already exists.' })
-    //         return res.redirect('../signup')
-    //       }
-    //       user.save((err) => {
-    //         if (err) { return next(err) }
-    //         req.logIn(user, (err) => {
-    //           if (err) {
-    //             return next(err)
-    //           }
-    //           res.redirect('/')
-    //         })
-    //       })
-    //     })
     }
-    
 }
