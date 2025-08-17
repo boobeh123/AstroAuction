ef2129b8d2e892936462bcffdcf4f48a951a3465
Bobby <boobeh321@gmail.com>
2025-08-16 20:14:19 -1000

    Update module.exports. Remove ensureLoginValidation & ensureAccountValidation. Require middleware directory

--- routes/main.js
+++ routes/main.js
@@ -5,34 +5,11 @@ const authController = require('../controllers/auth');
 const passport = require('passport')
 const { ensureLoginValidation, ensureAccountValidation } = require('../middleware/auth');
 
-
 router.get('/', homeController.getIndex);
-
 router.get('/login', authController.getLogin);
 router.get('/signup', authController.getSignup);
-
-// const ensureAccountValidation = [
-//     body('email')
-//     .isEmail().withMessage('Please enter a valid email address.')
-//     .normalizeEmail({ gmail_remove_dots: false }),
-//     body('password')
-//     .isLength({ min: 3 }).withMessage('Password must be at least 3 characters long'),
-//     body('confirmPassword')
-//     .custom((value, { req }) => {
-//         if (value !== req.body.password) throw new Error('Passwords do not match');
-//         return true;
-//     }),
-//     body('agreeToTerms')
-//     .equals('yes').withMessage('You must agree to the Terms of Use')
-// ];
 router.post('/signup', ensureAccountValidation, authController.postSignup);
-
-// const ensureLoginValidation = [
-//     body('email').isEmail().withMessage('Enter a valid email').normalizeEmail({ gmail_remove_dots: false }),
-//     body('password').notEmpty().withMessage('Password cannot be blank')
-// ];
 router.post('/login', ensureLoginValidation, authController.postLogin);
-
 router.get('/logout', authController.getLogout);
 
 module.exports = router;
