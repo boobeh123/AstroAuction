6e98a11d4b19efc1a9aaf92b263c53a8ff78db26
Bobby <boobeh321@gmail.com>
2025-08-18 21:01:54 -1000

    Update both validators - Add trim() to email & add key-value pair to isLength({}). Remove trim() & Remove escape() from password. Remove toLowerCase() - Do not mutate during validation.

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,23 +1,18 @@
 const { body } = require('express-validator');
 
 const ensureAccountValidation = [
-    body('email')
-    .isEmail().withMessage('Please enter a valid email address.')
-    .normalizeEmail({ gmail_remove_dots: false }),
-    body('password')
-    .isLength({ min: 3 }).withMessage('Password must be at least 3 characters long'),
-    body('confirmPassword')
-    .custom((value, { req }) => {
+    body('email').trim().isEmail().withMessage('Please enter a valid email address.').bail().normalizeEmail({ gmail_remove_dots: false }),
+    body('password').isLength({ min: 3, max: 128 }).withMessage('Password must be at least 3-128 characters'),
+    body('confirmPassword').custom((value, { req }) => {
         if (value !== req.body.password) throw new Error('Passwords do not match');
         return true;
     }),
-    body('agreeToTerms')
-    .equals('yes').withMessage('You must agree to the Terms of Use')
+    body('agreeToTerms').equals('yes').withMessage('You must agree to the Terms of Use')
 ];
 
 const ensureLoginValidation = [
-    body('email').toLowerCase().trim().isEmail().withMessage('Enter a valid email').normalizeEmail({ gmail_remove_dots: false }),
-    body('password').trim().notEmpty().withMessage('Password cannot be blank').isLength({ min: 3 }).withMessage('Password must be at least 3 characters').isLength({ max: 128 }).withMessage('Password cannot be longer than 128 characters').escape()
+    body('email').trim().isEmail().withMessage('Enter a valid email').bail().normalizeEmail({ gmail_remove_dots: false }),
+    body('password').notEmpty().withMessage('Password cannot be blank').isLength({ min: 3 }).withMessage('Password must be at least 3 characters').isLength({ max: 128 }).withMessage('Password cannot be longer than 128 characters')
 ];
 
 module.exports = { ensureLoginValidation, ensureAccountValidation };
