a7f13ecb590c3bf6dedcdf778057acba0e6a2793
Bobby <boobeh321@gmail.com>
2025-12-22 20:08:27 -1000

    Added postAuction method. Update getAuction method

--- controllers/auction.js
+++ controllers/auction.js
@@ -1,7 +1,31 @@
+const Auction = require('../models/Auction')
+
 module.exports = {
 
-    getAuction: (req, res) => {
-        res.render('auction.ejs');
-    }
+    getAuction: async (req, res) => {
+        try {
+            res.render('auction.ejs');
+        } catch(err) {
+            console.error(err)
+            // redirect 404 todo
+        }
+    },
+
+    postAuction: async (req, res) => {
+
+        try {
+            await Auction.create({
+                    title: req.body.title,
+                    description: req.body.description,
+                    image: req.body.image,
+                    video: req.body.video,
+                })
+                console.log('Listing has been added!')
+                res.redirect('/auction')
+            } catch(err) {
+                console.log(err)
+                // redirect 404 todo
+            }
+        },
     
 }
