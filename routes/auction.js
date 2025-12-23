4f3baf95493c473ac215d58dd2d6762a71504be6
Bobby <boobeh321@gmail.com>
2025-12-22 20:06:28 -1000

    Add POST request on '/'

--- routes/auction.js
+++ routes/auction.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const auctionController = require('../controllers/auction') 
 
 router.get('/', auctionController.getAuction);
+router.post('/', auctionController.postAuction);
 
 module.exports = router;
