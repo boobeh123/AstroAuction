b8b52b7da577025028cf9c2782fb04e89113bea4
Bobby <boobeh321@gmail.com>
2026-01-02 17:00:17 -1000

    Add POST request on /profile route & require multer

--- routes/profile.js
+++ routes/profile.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const profileController = require('../controllers/profile') 
+const upload = require("../middleware/multer");
 
 router.get('/', profileController.getProfile);
+router.post('/', upload.single('file'), profileController.uploadProfilePicture);
 
 module.exports = router;
