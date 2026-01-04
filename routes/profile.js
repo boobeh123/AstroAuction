a6937f36b487e0f1f279e8d055efd3477968e1a3
Bobby <boobeh321@gmail.com>
2026-01-03 17:38:54 -1000

    Add GET & PUT requests. Use ensureAuth to protect /profile, /profile/edit, /profile/edit/:id routes

--- routes/profile.js
+++ routes/profile.js
@@ -2,8 +2,11 @@ const express = require('express')
 const router = express.Router()
 const profileController = require('../controllers/profile') 
 const upload = require("../middleware/multer");
+const { ensureAuth } = require('../middleware/auth');
 
 router.get('/', profileController.getProfile);
-router.post('/', upload.single('file'), profileController.uploadProfilePicture);
+router.post('/', ensureAuth, upload.single('file'), profileController.uploadProfilePicture);
+router.get('/edit', ensureAuth, profileController.getEditProfile);
+router.put('/edit/:id', ensureAuth, profileController.updateProfile);
 
 module.exports = router;
