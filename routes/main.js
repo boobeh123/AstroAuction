b0dd791f3ad0e45794f47fce2cfb3624cdcf0aa7
Bobby <boobeh321@gmail.com>
2026-01-03 17:46:57 -1000

    Remove previous middleware

--- routes/main.js
+++ routes/main.js
@@ -3,13 +3,12 @@ const router = express.Router();
 const homeController = require('../controllers/home');
 const authController = require('../controllers/auth');
 const passport = require('passport')
-const { ensureLoginValidation, ensureAccountValidation } = require('../middleware/auth');
 
 router.get('/', homeController.getIndex);
 router.get('/login', authController.getLogin);
 router.get('/signup', authController.getSignup);
-router.post('/signup', ensureAccountValidation, authController.postSignup);
-router.post('/login', ensureLoginValidation, authController.postLogin);
+router.post('/signup', authController.postSignup);
+router.post('/login', authController.postLogin);
 router.get('/logout', authController.getLogout);
 
 module.exports = router;
