113c6e0dbdb83c1521fab47bea1a71879d215a32
Bobby <boobeh321@gmail.com>
2026-01-07 16:38:51 -1000

    Add DELETE request to /auction route

--- routes/auction.js
+++ routes/auction.js
@@ -4,5 +4,6 @@ const auctionController = require('../controllers/auction')
 
 router.get('/', auctionController.getAuction);
 router.post('/', auctionController.postAuction);
+router.delete('/deleteListing/:id', auctionController.deleteListing)
 
 module.exports = router;
