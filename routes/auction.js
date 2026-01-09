090b08adf939f48e7b652cfbc74165ab6be49d6c
Bobby <boobeh321@gmail.com>
2026-01-08 18:17:42 -1000

    Update POST request on /auction - add multer

--- routes/auction.js
+++ routes/auction.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const auctionController = require('../controllers/auction') 
+const upload = require("../middleware/multer");
 
 router.get('/', auctionController.getAuction);
-router.post('/', auctionController.postAuction);
+router.post('/', upload.single('file'), auctionController.postAuction);
 router.delete('/deleteListing/:id', auctionController.deleteListing)
 
 module.exports = router;
