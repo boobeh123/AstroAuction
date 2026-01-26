1297f6b19b7b438b5d5d8cb76b2952a7a37714eb
Bobby <boobeh321@gmail.com>
2026-01-26 11:46:39 -1000

    Add getTerms method

---
+++ controllers/term.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+    getTerms: async (req, res) => {
+
+        try {
+            res.render('terms.ejs');
+        } catch(err) {
+            console.error(err)
+            res.status(500).render('500.ejs');
+        }
+
+    }
+        
+}
