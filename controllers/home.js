dfe79a03c8198a5ba65b1d9ff76df3fee68dc3e2
Bobby <boobeh321@gmail.com>
2026-01-26 18:37:14 -1000

    Update getIndex method - remove passed in args

--- controllers/home.js
+++ controllers/home.js
@@ -4,10 +4,7 @@ module.exports = {
 
     getIndex: async (req, res) => {
         try {
-            res.render('index.ejs', {
-                user: req.user,
-                currentPath: req.originalUrl
-            });
+            res.render('index.ejs');
         } catch(err) {
             console.error(err)
             res.status(500).render('500.ejs');
