03d59e3100017f6a8b2a1ec8701e07b6263c926e
Bobby <boobeh321@gmail.com>
2026-01-27 13:12:09 -1000

    Add getPrivacy method to termController

--- controllers/term.js
+++ controllers/term.js
@@ -9,6 +9,17 @@ module.exports = {
             res.status(500).render('500.ejs');
         }
 
+    },
+
+    getPrivacy: async (req, res) => {
+
+        try {
+            res.render('privacy.ejs');
+        } catch(err) {
+            console.error(err)
+            res.status(500).render('500.ejs');
+        }
+
     }
         
 }
