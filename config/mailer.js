c7b3bc1527cc14a540c00ee412a9e049ae591718
Bobby <boobeh321@gmail.com>
2026-01-30 13:02:12 -1000

    Add port & secure properties

--- config/mailer.js
+++ config/mailer.js
@@ -3,6 +3,8 @@ const nodemailer = require('nodemailer');
 const createTransporter = () => {
   return nodemailer.createTransport({
     service: 'gmail',
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_NAME,
       pass: process.env.EMAIL_PASSWORD
