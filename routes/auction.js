c13dcb6f4fef5ab08ad71978f2dc09528432e3c8
Bobby <boobeh321@gmail.com>
2026-08-05 20:24:57 -1000

    Comment out several routes to restrict access

--- routes/auction.js
+++ routes/auction.js
@@ -3,10 +3,10 @@ const router = express.Router()
 const auctionController = require('../controllers/auction') 
 const upload = require("../middleware/multer");
 
-router.get('/', auctionController.getAuction);
-router.post('/', upload.single('file'), auctionController.postAuction);
-router.delete('/deleteAuction/:id', auctionController.deleteAuction);
+// router.get('/', auctionController.getAuction);
+// router.post('/', upload.single('file'), auctionController.postAuction);
+// router.delete('/deleteAuction/:id', auctionController.deleteAuction);
 
-router.get('/viewAuction/:id', auctionController.getDetailedAuction)
+// router.get('/viewAuction/:id', auctionController.getDetailedAuction)
 
 module.exports = router;
--- routes/main.js
+++ routes/main.js
@@ -9,17 +9,17 @@ router.get('/', homeController.getIndex);
 
 router.get('/login', authController.getLogin);
 router.get('/signup', authController.getSignup);
-router.post('/signup', authController.postSignup);
-router.post('/login', authController.postLogin);
-router.get('/logout', authController.getLogout);
-router.get('/verify/:token', authController.getVerified);
+// router.post('/signup', authController.postSignup);
+// router.post('/login', authController.postLogin);
+// router.get('/logout', authController.getLogout);
+// router.get('/verify/:token', authController.getVerified);
 router.get('/recover', authController.getForgetPassword);
-router.post('/recover', authController.postForgetPassword);
-router.get('/recover/:token', authController.getResetPassword);
-router.post('/recover/:token', authController.postResetPassword);
+// router.post('/recover', authController.postForgetPassword);
+// router.get('/recover/:token', authController.getResetPassword);
+// router.post('/recover/:token', authController.postResetPassword);
 
-router.get('/onboard', onboardController.getOnboard);
-router.post('/onboard', onboardController.postOnboard);
+// router.get('/onboard', onboardController.getOnboard);
+// router.post('/onboard', onboardController.postOnboard);
 
 router.get('/terms', termController.getTerms);
 router.get('/privacy', termController.getPrivacy);
