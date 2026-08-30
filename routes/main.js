d2edd3b01d92c0224a55ea9b8c72ba4640c675d9
Bobby <boobeh321@gmail.com>
2026-08-29 16:07:22 -1000

    Uncomment /signup & /onboard routes. Destructure ensureAuth & protect routes

--- routes/main.js
+++ routes/main.js
@@ -2,14 +2,16 @@ const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/home');
 const authController = require('../controllers/auth');
-// const onboardController = require('../controllers/onboard');
+const onboardController = require('../controllers/onboard');
 const termController = require('../controllers/term');
+const { ensureAuth } = require('../middleware/auth');
+
 
 router.get('/', homeController.getIndex);
 
 router.get('/login', authController.getLogin);
 router.get('/signup', authController.getSignup);
-// router.post('/signup', authController.postSignup);
+router.post('/signup', authController.postSignup);
 router.post('/login', authController.postLogin);
 router.get('/logout', authController.getLogout);
 // router.get('/verify/:token', authController.getVerified);
@@ -18,8 +20,8 @@ router.get('/recover', authController.getForgetPassword);
 // router.get('/recover/:token', authController.getResetPassword);
 // router.post('/recover/:token', authController.postResetPassword);
 
-// router.get('/onboard', onboardController.getOnboard);
-// router.post('/onboard', onboardController.postOnboard);
+router.get('/onboard', ensureAuth, onboardController.getOnboard);
+router.post('/onboard', ensureAuth, onboardController.postOnboard);
 
 router.get('/terms', termController.getTerms);
 router.get('/privacy', termController.getPrivacy);
