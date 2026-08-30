4f87e612cfcb7e9ae82b002ca4d9358e70204fe2
Bobby <boobeh321@gmail.com>
2026-08-30 12:37:34 -1000

    Add POST request on /profile/resend-verification , import auth controller

--- routes/profile.js
+++ routes/profile.js
@@ -3,11 +3,14 @@ const router = express.Router()
 const profileController = require('../controllers/profile') 
 const upload = require("../middleware/multer");
 const { ensureAuth } = require('../middleware/auth');
+const authController = require('../controllers/auth');
 
 router.get('/', ensureAuth, profileController.getProfile);
 router.post('/', ensureAuth, upload.single('file'), profileController.uploadProfilePicture);
 router.get('/edit', ensureAuth, profileController.getEditProfile);
 router.put('/edit/:id', ensureAuth, profileController.updateProfile);
 router.delete('/delete/:id', ensureAuth, profileController.deleteProfile);
+
+router.post('/resend-verification', ensureAuth, authController.postResendVerification);
 
 module.exports = router;
