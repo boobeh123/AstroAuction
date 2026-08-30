99e6bcfeb2a2d773e8ef560da042ed03fc31f9e5
Bobby <boobeh321@gmail.com>
2026-08-29 23:27:31 -1000

    Protect GET request on / route, uncomment several routes

--- routes/profile.js
+++ routes/profile.js
@@ -4,10 +4,10 @@ const profileController = require('../controllers/profile')
 const upload = require("../middleware/multer");
 const { ensureAuth } = require('../middleware/auth');
 
-// router.get('/', profileController.getProfile);
-// router.post('/', ensureAuth, upload.single('file'), profileController.uploadProfilePicture);
-// router.get('/edit', ensureAuth, profileController.getEditProfile);
-// router.put('/edit/:id', ensureAuth, profileController.updateProfile);
-// router.delete('/delete/:id', ensureAuth, profileController.deleteProfile);
+router.get('/', ensureAuth, profileController.getProfile);
+router.post('/', ensureAuth, upload.single('file'), profileController.uploadProfilePicture);
+router.get('/edit', ensureAuth, profileController.getEditProfile);
+router.put('/edit/:id', ensureAuth, profileController.updateProfile);
+router.delete('/delete/:id', ensureAuth, profileController.deleteProfile);
 
 module.exports = router;
