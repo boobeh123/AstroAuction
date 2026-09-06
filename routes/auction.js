c57b6fdab939284d111896f4e2173aae275395fc
Bobby <boobeh321@gmail.com>
2026-09-05 16:16:04 -1000

    Destructure ensureAuth, Protect POST & DELETE request routes, Update multer method for multi-image upload support

--- routes/auction.js
+++ routes/auction.js
@@ -2,11 +2,12 @@ const express = require('express')
 const router = express.Router()
 const auctionController = require('../controllers/auction') 
 const upload = require("../middleware/multer");
+const { ensureAuth } = require('../middleware/auth');
 
-// router.get('/', auctionController.getAuction);
-// router.post('/', upload.single('file'), auctionController.postAuction);
-// router.delete('/deleteAuction/:id', auctionController.deleteAuction);
+router.get('/', auctionController.getAuction);
+router.post('/', ensureAuth, upload.array('file', 10), auctionController.postAuction);
+router.delete('/deleteAuction/:id', ensureAuth, auctionController.deleteAuction);
 
-// router.get('/viewAuction/:id', auctionController.getDetailedAuction)
+router.get('/viewAuction/:id', auctionController.getDetailedAuction)
 
 module.exports = router;
