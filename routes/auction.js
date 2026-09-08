a3591818d6258cf90a6bea2cacffd41b423adcda
Bobby <boobeh321@gmail.com>
2026-09-07 19:15:03 -1000

    Add variable & update POST requests on /auction to call handleUploadErrors

--- routes/auction.js
+++ routes/auction.js
@@ -2,10 +2,11 @@ const express = require('express')
 const router = express.Router()
 const auctionController = require('../controllers/auction') 
 const upload = require("../middleware/multer");
+const handleUploadErrors = require("../middleware/handleUploadErrors");
 const { ensureAuth } = require('../middleware/auth');
 
 router.get('/', auctionController.getAuction);
-router.post('/', ensureAuth, upload.array('file', 10), auctionController.postAuction);
+router.post('/', ensureAuth, upload.array('file', 10), handleUploadErrors('/auction'), auctionController.postAuction);
 router.delete('/deleteAuction/:id', ensureAuth, auctionController.deleteAuction);
 
 router.get('/viewAuction/:id', auctionController.getDetailedAuction)
