e1e8789f4db258950a96a95261e1d9ca5576502e
Bobby <boobeh321@gmail.com>
2026-09-09 22:02:31 -1000

    Add POST request on /toggleSpotlight/:id , Rename routes for 2 POST requests, add ensureAuctioneer

--- routes/auction.js
+++ routes/auction.js
@@ -3,14 +3,15 @@ const router = express.Router()
 const auctionController = require('../controllers/auction') 
 const upload = require("../middleware/multer");
 const handleUploadErrors = require("../middleware/handleUploadErrors");
-const { ensureAuth } = require('../middleware/auth');
+const { ensureAuth, ensureAuctioneer } = require('../middleware/auth');
 
 router.get('/', auctionController.getAuction);
 router.post('/', ensureAuth, upload.array('file', 10), handleUploadErrors('/auction'), auctionController.postAuction);
 router.delete('/deleteAuction/:id', ensureAuth, auctionController.deleteAuction);
 
 router.get('/viewAuction/:id', auctionController.getDetailedAuction)
-router.post('/viewAuction/:id/comments', ensureAuth, auctionController.postComment);
-router.post('/viewAuction/:id/bids', ensureAuth, auctionController.postBid);
+router.post('/postComment/:id', ensureAuth, auctionController.postComment);
+router.post('/postBid/:id', ensureAuth, auctionController.postBid);
+router.post('/toggleSpotlight/:id', ensureAuth, ensureAuctioneer, auctionController.postToggleHighlight);
 
 module.exports = router;
