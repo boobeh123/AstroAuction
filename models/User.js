4d38435759285fcb2f7148105c5b322ce4ce26e6
Bobby <boobeh321@gmail.com>
2026-09-15 22:05:39 -1000

    Add tutorialStep property to UserSchema

--- models/User.js
+++ models/User.js
@@ -52,6 +52,10 @@ const UserSchema = new mongoose.Schema({
       type: String,
       default: 'user',
     },
+    tutorialStep: {
+      type: Number,
+      default: 0,
+    },
 },
     { timestamps: true }
 )
